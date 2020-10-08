2e036322046d3323d9cba559a1b28fe6c9a041a3
elvis <elvism72@hotmail.com>
2020-10-08 17:36:12 +0200

    ME_a lire

--- src/main/java/fr/afpa/cda/cda_projet_spacer_BaillardMessiaenDehorter/App.java
+++ src/main/java/fr/afpa/cda/cda_projet_spacer_BaillardMessiaenDehorter/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( " Les pc windows c'est de la merde" );
     }
 }
