e5d9f4eda62883b1bb0860e4ee18bdb3e3225eef
Cécile <cecile.baillard@laposte.net>
2020-10-14 20:06:11 +0200

    BC_updated meteorites : deleted useless code

--- src/main/java/fr/afpa/dao/beans/SimpleMeteoriteBeans.java
+++ src/main/java/fr/afpa/dao/beans/SimpleMeteoriteBeans.java
@@ -1,101 +1,12 @@
 package fr.afpa.dao.beans;
 
-import java.awt.Graphics;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class SimpleMeteoriteBeans extends MeteoriteBeans implements FlyingObject {
-
-	private ImageIcon iconMeteorite;
-	private Image imageMeteorite;
-	private int damage = 1;
-	private int meteoriteSize;
-	private int positionX;
-	private int positionY = 80;
-	private int speedX;
-	private int speedY = 2;
-	private int points = 2;
-	private boolean dead = false;
 	
 	public SimpleMeteoriteBeans(int positionX) {
 		
-		super();
+		super(positionX, "/meteorite.png");
 		
-		this.positionX = positionX;
-		
-		this.iconMeteorite = new ImageIcon(getClass().getResource("/meteorite.png"));
-		this.imageMeteorite = this.iconMeteorite.getImage();
-	}
-
-
-	@Override
-	public void move() {
-		
-		this.positionY += this.speedY;
-
-		if (this.positionX > GameConstants.GAME_SCREEN_MAX_WIDTH) {
-			this.positionX = GameConstants.GAME_SCREEN_MAX_WIDTH;
-			
-		} else if (this.positionX < GameConstants.GAME_SCREEN_MIN_WIDTH) {
-			this.positionX = GameConstants.GAME_SCREEN_MIN_WIDTH;
-		}
-
-		if (this.positionY > GameConstants.GAME_SCREEN_MAX_HEIGHT) {
-			this.positionY = GameConstants.GAME_SCREEN_MAX_HEIGHT;
-			this.dead = true;
-			
-		} else if (this.positionY < GameConstants.GAME_SCREEN_MIN_HEIGHT) {
-			this.positionY = GameConstants.GAME_SCREEN_MIN_HEIGHT;
-		}
-	}
-	
-	public void draw(Graphics graph) {
-		graph.drawImage(this.imageMeteorite, this.positionX, this.positionY, null);
-	}
-
-	public int getPositionX() {
-		return positionX;
-	}
-
-	public void setPositionX(int positionX) {
-		this.positionX = positionX;
-	}
-
-	public int getPositionY() {
-		return positionY;
-	}
-
-	public void setPositionY(int positionY) {
-		this.positionY = positionY;
-	}
-
-	public int getSpeedX() {
-		return speedX;
-	}
-
-	public void setSpeedX(int speedX) {
-		this.speedX = speedX;
-	}
-
-	public int getSpeedY() {
-		return speedY;
-	}
-
-	public void setSpeedY(int speedY) {
-		this.speedY = speedY;
-	}
-	
-	public int getDamage() {
-		return this.damage;
-	}
-	
-	public boolean isDead() {
-		return this.dead;
+		this.damage = 1;
+		this.points = 2;
 	}
 }
--- src/main/java/fr/afpa/dao/beans/ZigzagMeteoriteBeans.java
+++ src/main/java/fr/afpa/dao/beans/ZigzagMeteoriteBeans.java
@@ -1,48 +1,32 @@
 package fr.afpa.dao.beans;
 
-import java.awt.Graphics;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
-
 public class ZigzagMeteoriteBeans extends MeteoriteBeans implements FlyingObject{
-	
-	private ImageIcon iconMeteorite;
-	private Image imageMeteorite;
-	private int damage = 2;
-	private int meteoriteSize;
-	private int positionX;
-	private int goalX;
-	private int positionY = GameConstants.GAME_SCREEN_MIN_HEIGHT;
-	private int speedX = 2;
-	private int speedY = 1;
-	private double angle = 30.0;
-	private float amplitude = 8.0f;
-	private int points = 5;
-	private boolean dead = false;
-	
+
 	public ZigzagMeteoriteBeans(int positionX) {
 		
-		super();
+		super(positionX, "/zigzagMeteoriteLeft.png");
 		
 		this.positionX = positionX;
-		
-		this.iconMeteorite = new ImageIcon(getClass().getResource("/zigzagMeteoriteLeft.png"));
-		this.imageMeteorite = this.iconMeteorite.getImage();
+		this.positionY = GameConstants.GAME_SCREEN_MIN_HEIGHT;
+		this.speedX = 2;
+		this.speedY = 1;
+		this.damage = 2;
+		this.points = 5;
 	}
 
 	@Override
 	public void move() {
 		
+		int goalX = 0;
 
-		if (this.positionX >= GameConstants.GAME_SCREEN_MAX_WIDTH / 2) {
-			this.goalX = this.positionX - 20;
+		if (this.positionX > GameConstants.GAME_SCREEN_MAX_WIDTH / 2) {
+			goalX = this.positionX - 20;
 			this.positionX -= this.speedX;
 			this.positionY += this.speedY;
 		}
 		
-		if (this.positionX <= GameConstants.GAME_SCREEN_MAX_WIDTH / 2) {
-			this.goalX = this.positionX + 20;
+		if (this.positionX < GameConstants.GAME_SCREEN_MAX_WIDTH / 2) {
+			goalX = this.positionX + 20;
 			this.positionX += this.speedX;
 			this.positionY += this.speedY;
 		}
@@ -64,49 +48,5 @@ public class ZigzagMeteoriteBeans extends MeteoriteBeans implements FlyingObject
 		} else if (this.positionY < GameConstants.GAME_SCREEN_MIN_HEIGHT) {
 			this.positionY = GameConstants.GAME_SCREEN_MIN_HEIGHT;
 		}
-	}
-	
-	public void draw(Graphics graph) {
-		graph.drawImage(this.imageMeteorite, this.positionX, this.positionY, null);
-	}
-
-	public int getPositionX() {
-		return positionX;
-	}
-
-	public void setPositionX(int positionX) {
-		this.positionX = positionX;
-	}
-
-	public int getPositionY() {
-		return positionY;
-	}
-
-	public void setPositionY(int positionY) {
-		this.positionY = positionY;
-	}
-
-	public int getSpeedX() {
-		return this.speedX;
-	}
-
-	public void setSpeedX(int speedX) {
-		this.speedX = speedX;
-	}
-
-	public int getSpeedY() {
-		return speedY;
-	}
-
-	public void setSpeedY(int speedY) {
-		this.speedY = speedY;
-	}
-	
-	public int getDamage() {
-		return this.damage;
-	}
-	
-	public boolean isDead() {
-		return this.dead;
 	}
 }
