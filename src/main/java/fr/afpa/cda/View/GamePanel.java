bc50d3f201dc4afa4300ccc323190206818f65c9
Julien <ju.dehorter@gmail.com>
2020-10-15 17:31:36 +0200

    JD_Correction de la classe Scorecontroller afin qu'elle appelle directement les bonnes méthodes dans le métier

--- src/main/java/fr/afpa/cda/View/GamePanel.java
+++ src/main/java/fr/afpa/cda/View/GamePanel.java
@@ -141,8 +141,7 @@ public class GamePanel extends JPanel {
 
 		showScore(graph2);
 
-		String name =  String.valueOf ("Name : " + this.player.getName());	
-		System.out.println(name);
+		String name =  String.valueOf ("Name : " + this.player.getName());		
 		graph2.drawString(name, 250, 50);
 
 		String life = String.valueOf("HP : " + this.plane.getHealthPoints());
