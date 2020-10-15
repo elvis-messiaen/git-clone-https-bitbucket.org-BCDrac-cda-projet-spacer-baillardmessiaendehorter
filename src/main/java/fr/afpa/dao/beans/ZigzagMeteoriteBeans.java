afc5f3901f989ac9b650e64a68e7c6e04b760699
Cécile <cecile.baillard@laposte.net>
2020-10-16 00:23:00 +0200

    BC_Zigzag Meteorite finished (il etait temps !)

--- src/main/java/fr/afpa/dao/beans/ZigzagMeteoriteBeans.java
+++ src/main/java/fr/afpa/dao/beans/ZigzagMeteoriteBeans.java
@@ -1,12 +1,14 @@
 package fr.afpa.dao.beans;
 
+import javax.swing.ImageIcon;
+
 public class ZigzagMeteoriteBeans extends MeteoriteBeans implements FlyingObject {
 
 	int goalX;
 	int startX;
 	int steps;
 	int count;
-	boolean hasMovedOnce;
+	boolean left;
 
 	public ZigzagMeteoriteBeans(int positionX) {
 
@@ -20,88 +22,57 @@ public class ZigzagMeteoriteBeans extends MeteoriteBeans implements FlyingObject
 		this.damage = 2;
 		this.points = 5;
 		this.count = 0;
-		this.hasMovedOnce = false;
+		this.left = false;
 	}
 
 	@Override
 	public void move() {
 
-		/*
-		 * 
-		 * if (this.positionX > GameConstants.GAME_SCREEN_MAX_WIDTH) { this.positionX =
-		 * GameConstants.GAME_SCREEN_MAX_WIDTH;
-		 * 
-		 * } else if (this.positionX < GameConstants.GAME_SCREEN_MIN_WIDTH) {
-		 * this.positionX = GameConstants.GAME_SCREEN_MIN_WIDTH; }
-		 * 
-		 * 
-
-		 * if (this.goalX > GameConstants.GAME_SCREEN_MAX_WIDTH / 2) {
-		 * 
-		 * if (this.positionX > this.goalX && !this.hasMovedOnce) { this.positionX -=
-		 * this.speedX; this.positionY += this.speedY; }
-		 * 
-		 * if (this.positionX == this.goalX) { this.hasMovedOnce = true; }
-		 * 
-		 * if (this.positionX == this.goalX && this.hasMovedOnce) { this.positionX +=
-		 * this.speedX; this.positionY += this.speedY;
-		 * 
-		 * if (this.positionX == this.startX) { this.hasMovedOnce = false; } }
-		 * 
-		 * } else if (this.goalX > GameConstants.GAME_SCREEN_MAX_WIDTH / 2) {
-		 * 
-		 * if (this.positionX < this.goalX && !this.hasMovedOnce) { this.positionX +=
-		 * this.speedX; this.positionY += this.speedY; }
-		 * 
-		 * if (this.positionX == this.goalX) { this.hasMovedOnce = true; }
-		 * 
-		 * if (this.positionX == this.goalX && this.hasMovedOnce) { this.positionX -=
-		 * this.speedX; this.positionY += this.speedY;
-		 * 
-		 * if (this.positionX == this.startX) { this.hasMovedOnce = false; } } }
-		 */
-
-		this.positionX += this.speedX;
-		this.positionY += this.speedY;
-
-		// If knock a wall, revert horizontal speed
 		if (this.positionX <= GameConstants.GAME_SCREEN_MIN_WIDTH) {
 			this.positionX = GameConstants.GAME_SCREEN_MIN_WIDTH;
 
-			if (this.count <= 0) {
-				this.speedX -= speedX;
-				this.count++;
-			}
-
-		} else if (this.positionX >= GameConstants.GAME_SCREEN_MAX_WIDTH) { // Replace 600 by game screen // width
-			this.positionX = GameConstants.GAME_SCREEN_MAX_WIDTH;// Replace 600 by game screen width
-
-			if (this.count <= 0) {
-				this.speedX -= speedX;
-				this.count++;
-			}
+		} else if (this.positionX >= GameConstants.GAME_SCREEN_MAX_WIDTH) { // width
+			this.positionX = GameConstants.GAME_SCREEN_MAX_WIDTH;
 		}
 
-		if (this.count >= 2) {
-			this.speedX = -2;
-			this.count--;
-		}
-
+		
 		if (this.positionY > GameConstants.GAME_SCREEN_MAX_HEIGHT) {
 			this.positionY = GameConstants.GAME_SCREEN_MAX_HEIGHT;
 			this.dead = true;
+			System.out.println("Dead at : " + this.positionY);
 
 		} else if (this.positionY < GameConstants.GAME_SCREEN_MIN_HEIGHT) {
 			this.positionY = GameConstants.GAME_SCREEN_MIN_HEIGHT;
 		}
 
-		// bonus random speedX variation
-		/*
-		Random random = new Random();
-		if (random.nextInt(5 * 60) % 2 == 0) {// Randomly every 5 seconds
-			speedX = random.nextInt(5) - 2; // from -2 to + 2
+
+		this.count++;
+		this.positionY += this.speedY;
+
+		//Move left
+		if (this.left) {
+			if (this.count < 60) {
+				this.positionX -= this.speedX;
+				this.iconMeteorite = new ImageIcon(getClass().getResource("/zigzagMeteoriteLeft.png"));
+				this.imageMeteorite = this.iconMeteorite.getImage();
+
+			} else if (this.count >= 60) {
+				this.left = false;
+				this.count = 0;
+			}
+		
+		//Move right
+		} else {
+			if (this.count < 60) {
+				this.positionX += this.speedX;
+				this.iconMeteorite = new ImageIcon(getClass().getResource("/zigzagMeteoriteRight.png"));
+				this.imageMeteorite = this.iconMeteorite.getImage();
+
+			} else if (this.count >= 60) {
+				this.left = true;
+				this.count = 0;
+			}
 		}
-		*/
 
 	}
 }
